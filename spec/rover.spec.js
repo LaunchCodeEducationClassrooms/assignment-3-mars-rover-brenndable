d9e27619a07138f5976afb407050b469d3925dc3
brenprind <brennaprindable@gmail.com>
2021-03-19 18:21:04 +0000

    checking test 10 failures

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,61 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+// test 7
+
+  it ("constructor sets position and default values for mode and generalWatts", function() {
+    let rover = new Rover(98382)
+    expect(rover.position).toEqual(98382)
+    expect(rover.mode).toEqual("NORMAL")
+    expect(rover.generatorWatts).toEqual(110)
+  })
+
+// test 8
+  it ("response returned by receiveMessage contains name of message", function() {
+    let rover = new Rover(98382)
+    let commands = [new Command("MODE_CHANGE", "MOVE"), new Command("STATUS_CHECK")]
+    let message = new Message("Test Message", commands)
+    let response = rover.receiveMessage(message)
+    expect(response.message).toEqual("Test Message")
+  })
+
+// test 9
+  it ("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    let rover = new Rover(98382)
+    let commands = [ new Command("MODE_CHANGE", "MOVE"), new Command("STATUS_CHECK")]
+    let message = new Message("Test Message", commands)
+    let response = rover.receiveMessage(message)
+    expect((response.results).length).toEqual(2)
+    
+  })
+
+// test 10
+  it ("responds correctly to status check command", function() {
+    let rover = new Rover(98382)
+    let commands = [new Command("STATUS_CHECK")]
+    let message = new Message("Test Message", commands)
+    let response = rover.receiveMessage(message)
+    expect(response.results).toEqual([{completed: true, roverStatus:{mode: 'NORMAL',generatorWatts: 110, position: 98382}}])
+
+  })
+
+// test 11
+  it ("responds correctly to mode change command", function() {
+    let rover = new Rover(98382)
+    let commands = [new Command("MODE_CHANGE", "LOW_POWER")]
+    let message = new Message("Test Message", commands)
+    let response = rover.receiveMessage(message)
+  })
+
+// test 12
+  it ("responds with false completed value when attempting to move in LOW_POWER mode", function() {
+
+  })
+
+// test 13
+  it ("responds with position for move command", function() {
+
+  })
+
 
 });
